855dd81604ad5ff65d7c8dfd84b3fd5cb76a3b58
Ivan <pravdin.biss@gmail.com>
2022-11-06 18:00:23 +0400

    Add get-speakers and change-speaker route

--- alias_control_panel_server/routes.py
+++ alias_control_panel_server/routes.py
@@ -3,7 +3,6 @@ from flask import Flask, request, redirect, url_for, jsonify
 
 from utils import *
 from models import UserSession
-from yandex_api import YandexAPI
 
 @app.route('/auth', methods=['GET'])
 def authorize():
@@ -55,61 +54,40 @@ def logout():
     else:
         return create_unauthorized_response("You are not authorized")
 
-@app.route("/get_speakers", methods=['GET'])
+@app.route("/get-speakers", methods=['GET'])
 def get_speakers():
-    if (request.cookies.get('csrf_token')):
-        if not check_authorization("123"): # TODO
-            return redirect(url_for('logout'))
+    token = request.cookies.get(UserSession.token_name)
+    if (token):
+        session = session_store.get_session(token)
+        if not session:
+            response = create_unauthorized_response("Session for this user doesn't exist")
+            response.set_cookie(UserSession.token_name, '', max_age=0)
+            return response
 
-        session = get_session(request.cookies.get('csrf_token'))
-        if session is None:
-            return redirect(url_for('logout'))
+        res = create_success_response(content=jsonify([ s.__dict__ for s in session.get_speakers() ]))
 
-        print(session.get_speakers().__class__)
-
-        return create_success_response(jsonify(session.get_speakers()))
+        return res
     else:
-        return create_success_response()
+        return create_unauthorized_response("You are not authorized")
 
-devices = [
-    {
-      "image": "https://yastatic.net/s3/frontend/quasar/_/3624d5019c8d9b6d9601486da6c01bf3.png",
-      "name": 'Yandex Sasisa',
-      "used": True
-    },
-    {
-      "image": 'https://yastatic.net/s3/frontend/quasar/_/3624d5019c8d9b6d9601486da6c01bf3.png',
-      "name": 'Yandex Aziza',
-      "used": False
-    }
-  ]
-
-
-@app.route("/get-devices", methods=['GET'])
-def get_devices():
-
-    return create_success_response(content=jsonify(devices))
-    # if (request.cookies.get('csrf_token')):
-    #     return create_success_response(content=jsonify(devices))
-    # else:
-    #     return create_success_response()
-
-
-@app.route("/change-device", methods=['POST', 'OPTIONS'])
-def change_device():
-
+@app.route("/change-speaker", methods=['POST', 'OPTIONS'])
+def change_speaker():
     if request.method == "OPTIONS":
         return create_success_response()
 
+    token = request.cookies.get(UserSession.token_name)
+    if (token):
+        session = session_store.get_session(token)
+        if not session:
+            response = create_unauthorized_response("Session for this user doesn't exist")
+            response.set_cookie(UserSession.token_name, '', max_age=0)
+            return response
 
-    new_device = request.json
-    for d in devices:
-        if d["name"] == new_device["name"]:
-            d["used"] = True
-        else:
-            d["used"] = False
+        new_speaker = request.json
+        session.change_active_speaker(new_speaker)
 
-    return create_success_response(content=jsonify(devices))
+        res = create_success_response(content=jsonify([s.__dict__ for s in session.get_speakers()]))
 
-def get_session(token):
-    return next(filter(lambda x: x.csrf_token == token, users_sessions), None)
+        return res
+    else:
+        return create_unauthorized_response("You are not authorized")
